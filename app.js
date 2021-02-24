7fed6663f997d1fed18dbca7d8d9a231fe8fd739
huanginch <57096811+huanginch@users.noreply.github.com>
2021-02-24 16:38:22 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const route = require('./src/server/route')
+
+//引入dist
+app.use('/', express.static(path.join(__dirname,'dist')))
+
+//引入route
+app.use('/', route)
+app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, '../../dist/index.html'))
+})
+
+//listen on port 3000
+app.listen(3000,() => { 
+    console.log('app listening on port 3000.')
+}) 
