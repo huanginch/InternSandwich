2448ebf3c405872731dadfc22ba6cbb41e2ddb90
selena88huang <57096909+selena88huang@users.noreply.github.com>
2021-03-10 22:00:22 +0800

    0310開會
    三明治首頁

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -1,19 +1,253 @@
-<!--首頁-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App :))"/>
-  </div>
+    <div style="padding: 100px 100px 10px">
+        <form class="bs-example bs-example-form" role="form">
+          <div class="row align-items-center">
+            <div class="col-lg-12">
+              <div class="input-group mt-3 mb-3">
+                <input type="text" class="form-control" />
+                <div class="input-group-btn">
+                  <button
+                    type="button"
+                    class="btn btn-default dropdown-toggle"
+                    data-toggle="dropdown"
+                  >
+                    類別
+                    <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu pull-right">
+                    <li><a href="#">資訊科技</a></li>
+                    <li><a href="#">文書行政</a></li>
+                    <li><a href="#">工科設計</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">其他</a></li>
+                  </ul>
+                </div>
+                <div class="input-group-btn">
+                  <button
+                    type="button"
+                    class="btn btn-default dropdown-toggle"
+                    data-toggle="dropdown"
+                  >
+                    地點
+                    <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">台北</a></li>
+                    <li><a href="#">台中</a></li>
+                    <li><a href="#">台南</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">其他</a></li>
+                  </ul>
+                </div>
+                <div class="input-group-btn">
+                  <button
+                    type="button"
+                    class="btn btn-default dropdown-toggle"
+                    data-toggle="dropdown"
+                  >
+                    薪資
+                    <span class="caret"></span>
+                  </button>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">最低薪資</a></li>
+                    <li><a href="#">22000/month</a></li>
+                    <li><a href="#">28000/month</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">其他</a></li>
+                  </ul>
+                </div>
+                <button class="btn">
+                  <span class="glyphicon glyphicon-search"></span>搜尋<span
+                    class="caret"
+                  ></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        <br />
+
+       
+
+        <div class="row">
+          <div class="col-lg-9 text-left">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div class="ppp">
+                  <div class="photo">
+                    <img
+                      src="../assets/圖片1.png"
+                      alt="internsandwich"
+                      height="150px"
+                    />
+                  </div>
+                  <a style="font-size: 25px">OO實習生</a><br />
+                  <a style="font-size: 20px">OOO股份有限公司</a><br />
+                  <a style="font-size: 15px"
+                    >我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容我是工作內容</a
+                  ><br />
+                </div>
+
+                <div class="ppp2">
+                  <div class="row float-right">
+                    <a
+                      href="#"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                      >查看評論</a
+                    >
+                    <a
+                      href="#"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                      >收藏</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div class="ppp">
+                  <div class="photo">
+                    <img
+                      src="../assets/圖片1.png"
+                      alt="internsandwich"
+                      height="150px"
+                    />
+                  </div>
+                  <a style="font-size: 25px">採購實習生</a><br />
+                  <a style="font-size: 20px">全聯大賣場股份有限公司</a><br />
+                  <a style="font-size: 15px"
+                    >1.原物料採購(國內/進口料)、結帳作業。 2.供應商資料維護。
+                    3.收料、庫存管理。 4. 原料退稅作業。 5. 主管交辦事項。</a
+                  ><br />
+                </div>
+
+                <div class="ppp2">
+                  <div class="row float-right">
+                    <a
+                      href="#"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                      >查看評論</a
+                    >
+                    <a
+                      href="#"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                      >收藏</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div class="ppp">
+                  <div class="photo">
+                    <img
+                      src="../assets/圖片1.png"
+                      alt="internsandwich"
+                      height="150px"
+                    />
+                  </div>
+                  <a style="font-size: 25px">資安實習生</a><br />
+                  <a style="font-size: 20px">中正大學研發處</a><br />
+                  <a style="font-size: 15px"
+                    >協助資安檢測工具研發，包含分析、設計、程式撰寫及軟體測試 -
+                    協助軟體開發文件之撰寫與維護 -
+                    協助新技術、法規與產品的研究</a
+                  ><br />
+                </div>
+
+                <div class="ppp2">
+                  <div class="row float-right">
+                    <a
+                      href="#"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                      >查看評論</a
+                    >
+                    <a
+                      href="#"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                      >收藏</a
+                    >
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 text-left">
+            <div class="photo">
+              <img
+                src="../assets/圖片1.png"
+                alt="internsandwich"
+                height="100px"
+              />
+            </div>
+            <div class="intro">
+              <a>OOO股份有限公司</a><br />
+              <a>OOO實習生</a>
+            </div>
+
+            <div class="photo">
+              <img
+                src="../assets/圖片1.png"
+                alt="internsandwich"
+                height="100px"
+              />
+            </div>
+            <div class="intro">
+              <a>OOO股份有限公司</a><br />
+              <a>OOO實習生</a>
+            </div>
+
+            <div class="photo">
+              <img
+                src="../assets/圖片1.png"
+                alt="internsandwich"
+                height="100px"
+              />
+            </div>
+            <div class="intro">
+              <a>OOO股份有限公司</a><br />
+              <a>OOO實習生</a>
+            </div>
+
+            <div class="photo">
+              <img
+                src="../assets/圖片1.png"
+                alt="internsandwich"
+                height="100px"
+              />
+            </div>
+            <div class="intro">
+              <a>OOO股份有限公司</a><br />
+              <a>OOO實習生</a>
+            </div>
+          </div>
+        </div>
+
+        <br />
+
+        <ul
+          class="pagination pagination-sm justify-content-center"
+          style="margin: 20px 0"
+        >
+          <li class="page-item disabled">
+            <a class="page-link" href="#">上一頁</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">下一頁</a></li>
+        </ul>
+      </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/client/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
