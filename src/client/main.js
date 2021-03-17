be1a7e9beee79aedac546d2dd50fe52f3d628b94
selena88huang <57096909+selena88huang@users.noreply.github.com>
2021-03-17 22:04:49 +0800

    0317開會

--- src/client/main.js
+++ src/client/main.js
@@ -1,15 +1,27 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
-import bootstrap from './assets/bootstrap.min.css'
+import bootstrap_css from './assets/bootstrap.min.css'
+// import bootstrap_js from './assets/bootstrap.min.js'
+import popper from 'popper.js'
+import $ from 'jquery'
+
+import bootstrap_vue_css from '../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue)
+
+window.$ = $
 
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  bootstrap,
+  bootstrap_css,
+  bootstrap_vue_css,
+  popper,
   render: h => h(App)
 }).$mount('#app')
