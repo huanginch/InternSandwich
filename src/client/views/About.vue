ab54c375ee68ed218af03dd33b42dba511c87f39
selena88huang <57096909+selena88huang@users.noreply.github.com>
2021-03-18 20:00:33 +0800

    0318重新上傳views

--- src/client/views/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-<div style="padding: 100px 100px 10px">
-  <div class="about">
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-    <h1>This is an about page</h1>
-  
-  </div></div>
-</template>
