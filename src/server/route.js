375421f217246cac94360cdd352ffd879e5e3fdc
huanginch <57096811+huanginch@users.noreply.github.com>
2021-03-18 21:20:04 +0800

    update router.js

--- src/server/route.js
+++ src/server/route.js
@@ -1,11 +1,43 @@
 //express路由
 const { Router } = require('express')
 const router = Router()
-const user = require('./db/user')
+const {db} = require('./db/db.js')
 
-router.get('/api/test', async (req, res, next) => {
-  const user1 = await user.getUserbyAccount('test1')
-  res.send(user1)
+
+router.get('/posts', (req, res, next) => {
+  /*const param = req.params.uAccount
+  const user1 = await user.getUserbyAccount(param)
+  res.send(user1)*/
+  const sql = 'select * from fb_post'
+  db(sql)
+  .then(results =>{
+    res.send(results);
+  })
+  .catch(err =>{
+    res.status(500).send("err:",err)
+  })
+})
+
+router.get('/users', (req, res, next) => {
+  const sql = 'select * from users'
+  db(sql)
+  .then(results =>{
+    res.send(results);
+  })
+  .catch(err =>{
+    res.status(500).send("err:",err)
+  })
+})
+
+router.post('/users', (req, res, next) => {
+  const sql = 'insert values * from users'
+  db(sql)
+  .then(results =>{
+    res.send(results);
+  })
+  .catch(err =>{
+    res.status(500).send("err:",err)
+  })
 })
 
 module.exports = router
