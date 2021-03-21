b9449da6c538ecd6abc105e222328961d34b4c20
huanginch <57096811+huanginch@users.noreply.github.com>
2021-03-21 16:33:21 +0800

    solve the cors problem

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 //專案進入點
 const express = require('express')
+const cookieParser = require('cookie-parser')
 const path = require('path')
 const app = express()
 const route = require('./src/server/route')
+const cors = require('cors')
+
+app.use(cors({origin:"http://localhost:8080",credentials:true}))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cookieParser())
 
 //引入dist
 app.use('/', express.static(path.join(__dirname,'dist')))
 
 //引入route
-app.use('/', route)
+app.use('/api', route)
 app.use((req, res) => {
   res.sendFile(path.resolve(__dirname, '../../dist/index.html'))
 })
