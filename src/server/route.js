bf7962f3314b76e906cf07cca8652c96f84f4061
huanginch <57096811+huanginch@users.noreply.github.com>
2021-03-25 11:18:10 +0800

    add middleware folder and update route.js
    add post('user-login') and post('user-register')

--- src/server/route.js
+++ src/server/route.js
@@ -1,13 +1,14 @@
-//express路由
-const { Router } = require('express')
-const router = Router()
-const {db} = require('./db/db.js')
+//封裝express後端路由
+const { Router } = require('express');
+const router = Router();
+const {db} = require('./db/db.js');
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const userMiddleware = require('./middleware/users.js');
 
+//貼文
 router.get('/posts', (req, res, next) => {
-  /*const param = req.params.uAccount
-  const user1 = await user.getUserbyAccount(param)
-  res.send(user1)*/
   const sql = 'select * from fb_post'
   db(sql)
   .then(results =>{
@@ -18,26 +19,86 @@ router.get('/posts', (req, res, next) => {
   })
 })
 
-router.get('/users', (req, res, next) => {
-  const sql = 'select * from users'
-  db(sql)
+//一般使用者登入
+router.post('/user-login', (req, res, next) => {
+  const params = escape(req.body.account);
+  const sql = 'SELECT * FROM user_info WHERE Account = ?'
+  db(sql,params)
   .then(results =>{
-    res.send(results);
+    if(!results.length){
+      res.status(401).send({msg:"帳號錯誤"})
+    }
+
+    bcrypt.compare(
+      req.body.password,
+      results[0]['Password'],
+      function (bErr, bResult) {
+        if (bErr) {
+         return res.status(401).send({msg:"密碼錯誤"});
+        }
+        if (bResult) {
+          const token = jwt.sign({
+              account: results[0]['Account'],
+              userId: results[0]['ID']
+            },
+            'SECRETKEY', {
+              expiresIn: '7d'//token時效
+            });
+
+           return res.status(200).send({
+              msg: '登入成功!',
+              token,
+              user: results[0]
+            });
+          }
+
+        return res.status(401).send({msg:"密碼錯誤"});
+        }
+    );
   })
   .catch(err =>{
-    res.status(500).send("err:",err)
+    res.status(400).send("err:",err)
   })
 })
 
-router.post('/users', (req, res, next) => {
-  const sql = 'insert values * from users'
-  db(sql)
+//一般使用者註冊
+router.post('/user-register', userMiddleware.validateRegister, (req, res, next) => {
+  const params = escape(req.body.account);
+  var sql = "SELECT * FROM user_info WHERE Account = ?"
+  db(sql, params)
   .then(results =>{
-    res.send(results);
+    if (results.length) {
+      res.status(409).send({msg:"此帳號已被使用"});
+    }
+    else{
+      // username is available
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          res.status(500).send({msg: err}); 
+        } 
+        else {
+          // has hashed pw => add to database
+          const params = [escape(req.body.name), req.body.gender, escape(req.body.phone), escape(req.body.email), escape(req.body.account), hash];
+          sql = "INSERT INTO user_info (Name, Gender, Phone, Email, Account, Password) VALUES (?, ?, ?, ?, ?, ?);"
+          db(sql,params)
+          .then(result => {
+            res.status(201).send({msg:"註冊成功"})
+          })
+          .catch(err =>{
+            res.status(400).send("err:",err)
+          })
+        }
+      })
+    }
   })
   .catch(err =>{
-    res.status(500).send("err:",err)
+    res.status(400).send("err:",err)
   })
 })
+
+router.get('/secret-route', userMiddleware.isLoggedIn, (req, res, next) => {
+  console.log(req.userData);
+  res.send('This is the secret content. Only logged in users can see that!');
+});
 
 module.exports = router
