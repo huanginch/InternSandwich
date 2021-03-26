e3703d23028a9888ef2d3f19181441219c09d8ee
huanginch <57096811+huanginch@users.noreply.github.com>
2021-03-26 21:12:16 +0800

    update main.js
    add jwt and vuex

--- src/client/main.js
+++ src/client/main.js
@@ -1,9 +1,12 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import store from './store';
+//import VueRouter from 'vue-router'
 // import './plugins/bootstrap-vue'
 import App from './App.vue'
 import router from './router'
+import axios from './js/axios.js';
 import bootstrap_css from './assets/bootstrap.min.css'
 // import bootstrap_js from './assets/bootstrap.min.js'
 //import popper from 'popper.js'
@@ -12,14 +15,21 @@ import bootstrap_css from './assets/bootstrap.min.css'
 import bootstrap_vue_css from '../../node_modules/bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from '../../node_modules/bootstrap-vue/dist/bootstrap-vue.js'
 Vue.use(BootstrapVue)
+// Vue.use(VueRouter)
+// //window.$ = $
 
-//window.$ = $
 
-
+// const router = new VueRouter({
+//   routes,
+//   mode: 'history'
+// });
+// set auth header
+axios.defaults.headers.common['Authorization'] = `Bearer ${store.state.token}`;
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   bootstrap_css,
   bootstrap_vue_css,
   render: h => h(App)
