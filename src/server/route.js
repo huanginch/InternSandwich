82b26886aa8f899b8a447e8a9bf501713d5384d5
Shen-KuanLing <44127801+Shen-KuanLing@users.noreply.github.com>
2021-03-30 21:15:12 +0800

    edit login/register's db info

--- src/server/route.js
+++ src/server/route.js
@@ -9,7 +9,7 @@ const userMiddleware = require('./middleware/users.js');
 
 //貼文
 router.get('/posts', (req, res, next) => {
-  const sql = 'select * from web_post'
+  const sql = 'select * from intern_post'
   db(sql)
   .then(results =>{
     res.send(results);
@@ -21,24 +21,24 @@ router.get('/posts', (req, res, next) => {
 
 //一般使用者登入
 router.post('/user-login', (req, res, next) => {
-  const params = escape(req.body.account);
-  const sql = 'SELECT * FROM user_info WHERE Account = ?'
+  const params = escape(req.body.email);
+  const sql = 'SELECT * FROM user_info WHERE email = ?'
   db(sql,params)
   .then(results =>{
     if(!results.length){
-      res.status(401).send({msg:"帳號錯誤"})
+      res.status(401).send({msg:"email錯誤"})
     }
 
     bcrypt.compare(
       req.body.password,
-      results[0]['Password'],
+      results[0]['password'],
       function (bErr, bResult) {
         if (bErr) {
          return res.status(401).send({msg:"密碼錯誤"});
         }
         if (bResult) {
           const token = jwt.sign({
-              account: results[0]['Account'],
+              account: results[0]['email'],
               userId: results[0]['ID']
             },
             'SECRETKEY', {
@@ -63,8 +63,9 @@ router.post('/user-login', (req, res, next) => {
 
 //一般使用者註冊
 router.post('/user-register', userMiddleware.validateRegister, (req, res, next) => {
-  const params = escape(req.body.account);
-  var sql = "SELECT * FROM user_info WHERE Account = ?"
+  const params = escape(req.body.email);
+  //var sql = "SELECT * FROM user_info WHERE Account = ?"
+  var sql = "SELECT * FROM user_info WHERE email = ?"
   db(sql, params)
   .then(results =>{
     if (results.length) {
@@ -78,8 +79,8 @@ router.post('/user-register', userMiddleware.validateRegister, (req, res, next) 
         } 
         else {
           // has hashed pw => add to database
-          const params = [escape(req.body.name), req.body.gender, escape(req.body.phone), escape(req.body.email), escape(req.body.account), hash];
-          sql = "INSERT INTO user_info (Name, Gender, Phone, Email, Account, Password) VALUES (?, ?, ?, ?, ?, ?);"
+          const params = [escape(req.body.name), req.body.gender, escape(req.body.phone), escape(req.body.email), hash, escape(req.body.id_card), escape(req.body.birth), escape(req.body.school)];
+          sql = "INSERT INTO user_info (name, gender, phone, email, password, id_card, birth, school) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
           db(sql,params)
           .then(result => {
             res.status(201).send({msg:"註冊成功"})
