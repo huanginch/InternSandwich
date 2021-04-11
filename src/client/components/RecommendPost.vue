345f6272128f6be124ddbcfb925d843d3e617fa4
selena88huang <57096909+selena88huang@users.noreply.github.com>
2021-04-12 00:29:33 +0800

    0411更新

--- src/client/components/RecommendPost.vue
+++ src/client/components/RecommendPost.vue
@@ -1,52 +1,57 @@
 <template>
-<div class="col-lg-3 text-left">
-  <p class="text-left">
-    <strong>{{title}}</strong>
-  </p>
+  <div class="col-lg-3 text-left">
+    <p class="text-left">
+      <strong>{{ title }}</strong>
+    </p>
     <div id="Recommend" v-for="posts in recommend_posts" :key="posts.id">
-    <router-link :to="{ name: 'Intern', params: { post_id: posts.id }}" class="routerlink">
-    <div class="panel">
-      <div class="panel-body">
-        <div class="row">
-          <div class="col-lg-3">
-            <img
-              src="../assets/圖片1.png"
-              alt="internsandwich"
-              style="height: 70px"
-            />
-          </div>
-          <div class="col-lg-9">
-            <p style="font-size: 20px" align="left">{{posts.title}}</p>
-            <p style="font-size: 20px" align="left">{{posts.cp_name}}</p>
+      <router-link
+        :to="{ name: 'Intern', params: { post_id: posts.id } }"
+        class="routerlink"
+      >
+        <div class="panel_popular">
+          <div class="panel-body">
+            <div class="row">
+              <div class="col-lg-3">
+                <img
+                  src="../assets/圖片1.png"
+                  alt="internsandwich"
+                  style="height: 70px"
+                />
+              </div>
+              <div class="col-lg-9">
+                <p style="font-size: 20px" align="left">{{ posts.title }}</p>
+                <p style="font-size: 20px" align="left">{{ posts.cp_name }}</p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+      </router-link>
+      <hr border-style="solid" />
     </div>
-  </router-link>
   </div>
-
-</div>
 </template>
 
 <script>
 export default {
-  name: 'Recommend',
+  name: "Recommend",
   props: {
     recommend_posts: {
       type: Array,
-      required: true
+      required: true,
     },
     title: {
       type: String,
-      required: true
+      required: true,
     },
   },
-}
+};
 </script>
 
 <style>
- .routerlink{ 
+.routerlink {
   color: black;
 }
- 
+#panel_popular {
+  background-color: #ebf2ff54;
+}
 </style>
