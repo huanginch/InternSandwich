fdc4e517857b541c60b3036e52a5289c2f9093c8
selena88huang <57096909+selena88huang@users.noreply.github.com>
2021-04-13 00:02:19 +0800

    0412更新

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -26,7 +26,7 @@
               </option>
             </select>
             <select v-model="select_area" class="btn btn-default">
-              <option  value="">地區</option>
+              <option value="">地區</option>
               <!--eslint-disable-next-line-->
               <option v-for="area in areas" v-bind:value="area.value">
                 {{ area.text }}
@@ -47,17 +47,32 @@
     <!--實習貼文與熱門搜尋  -->
     <div class="row">
       <div class="col-lg-9">
-        <button class="btn float-right" style="font-size:17px" @click="sortNewtoOld">由新到舊</button>
-        <button class="btn float-right" style="font-size:17px" @click="sortOldtoNew">由舊到新</button>
+        <!-- <button class="btn float-right" style="font-size:17px" @click="sortNewtoOld">觀看次數由多到少</button>
+        <button class="btn float-right" style="font-size:17px" @click="sortOldtoNew">觀看次數由少到多</button> -->
+        <button
+          class="btn float-right"
+          style="font-size: 17px"
+          @click="sortNewtoOld"
+        >
+          由新到舊
+        </button>
+        <button
+          class="btn float-right"
+          style="font-size: 17px"
+          @click="sortOldtoNew"
+        >
+          由舊到新
+        </button>
         <br />
-        
+
         <!-- 實習貼文 -->
         <!--eslint-disable-next-line-->
         <div id="Home" v-for="(posts, index) in searchResult.slice(pageStart, pageStart + countOfPage)" class="posts" >
           <div class="panel panel-default">
-            <div class="panel-body" style="border-style: ridge"><!-- ridge groove inset outset -->
-            <router-link class="nav-item nav-link" :to="{ name: 'Intern', params: { post_id: posts.id }}"
-            @click.native="addcounter(posts.id, posts.counter)">
+            <div class="panel-body" style="border-style: ridge">
+              <!-- ridge groove inset outset -->
+              <!-- <router-link class="nav-item nav-link" :to="{ name: 'Intern', params: { post_id: posts.id }}"
+            @click.native="addcounter(posts.id, posts.counter)"> -->
               <div class="row">
                 <div class="col-lg-2">
                   <img
@@ -90,38 +105,107 @@
                 </div>
               </div>
               <div class="ppp2_btn">
-                <div class="row float-left" style="color: grey; padding-top: 20px">
-                  觀看次數: {{posts.counter}}
+                <div
+                  class="float-left"
+                  style="
+                    font-size: 20px;
+                    color: grey;
+                    padding-top: 12px;
+                    padding-left: 5px;
+                  "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="32"
+                    fill="currentColor"
+                    class="bi bi-eye-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                    <path
+                      d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"
+                    />
+                  </svg>
+                  觀看次數: {{ posts.counter }}
                 </div>
                 <div class="row float-right">
                   <router-link
                     class="btn"
                     style="width: 200px; height: 50px; font-size: 20px"
-                    :to="{ name: 'Intern', params: { post_id: posts.id }}"
+                    :to="{ name: 'Intern', params: { post_id: posts.id } }"
                   >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="18"
+                      fill="currentColor"
+                      class="bi bi-chat-square-text"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+                      />
+                      <path
+                        d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+                      />
+                    </svg>
                     查看評論
                   </router-link>
-                  <div v-bind:class="{hidden: saved_posts.indexOf(posts.id) != -1}">
+                  <div
+                    v-bind:class="{
+                      hidden: saved_posts.indexOf(posts.id) != -1,
+                    }"
+                  >
                     <button
-                      @click='save(posts.id)'
+                      @click="save(posts.id)"
                       class="btn"
                       style="width: 200px; height: 50px; font-size: 20px"
                     >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="18"
+                        fill="currentColor"
+                        class="bi bi-suit-heart"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"
+                        />
+                      </svg>
                       收藏
                     </button>
                   </div>
-                  <div v-bind:class="{hidden: saved_posts.indexOf(posts.id) === -1}">
+                  <div
+                    v-bind:class="{
+                      hidden: saved_posts.indexOf(posts.id) === -1,
+                    }"
+                  >
                     <button
                       @click="unsave(posts.id)"
                       class="btn"
                       style="width: 200px; height: 50px; font-size: 20px"
                     >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="18"
+                        fill="currentColor"
+                        class="bi bi-suit-heart-fill"
+                        color="red"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                        />
+                      </svg>
                       取消收藏
                     </button>
                   </div>
                 </div>
               </div>
-              </router-link>
+              <!-- </router-link> -->
             </div>
           </div>
         </div>
@@ -203,8 +287,8 @@
       <!--熱門搜尋  -->
       <!--<div class="col-lg-3">
         <p class="text-left"><strong>熱門實習</strong></p>-->
-        <!--eslint-disable-next-line-->
-        <!--<div id="Home" v-for="(posts, index) in top5_post" class="posts" >
+      <!--eslint-disable-next-line-->
+      <!--<div id="Home" v-for="(posts, index) in top5_post" class="posts" >
           <div class="panel">
             <div class="panel-body">
               <div class="row">
@@ -259,11 +343,11 @@
 </template>
 <script>
 import axios from "../js/axios.js";
-import RecommendPost from "../components/RecommendPost.vue"
+import RecommendPost from "../components/RecommendPost.vue";
 export default {
   name: "Home",
-  components:{
-    RecommendPost
+  components: {
+    RecommendPost,
   },
   data() {
     return {
@@ -285,7 +369,7 @@ export default {
         { text: "台中", value: "台中" },
         { text: "高雄", value: "高雄" },
       ],
-      saved_posts:[]
+      saved_posts: [],
     };
   },
   computed: {
@@ -297,26 +381,26 @@ export default {
     totalPage: function () {
       return Math.ceil(this.searchResult.length / this.countOfPage);
     },
-    isLoggedIn: function(){
+    isLoggedIn: function () {
       return this.$store.getters.isLoggedIn;
     },
   },
   methods: {
     //更新觀看次數
     addcounter: function (p_id, counter) {
-      counter++
-      var api = "/api/add-counter"
+      counter++;
+      var api = "/api/add-counter";
       const params = {
         p_id: p_id,
-        counter: counter
-      }
+        counter: counter,
+      };
       axios
         .post(api, params)
         .then((response) => {
-          alert(response.data.msg)
+          alert(response.data.msg);
         })
         .catch((error) => {
-          console.log(error)
+          console.log(error);
         });
     },
     //依照關鍵字搜尋貼文
@@ -328,40 +412,21 @@ export default {
       var select_area = this.select_area;
 
       // 如果 filter_name 有內容，回傳過濾後的資料，否則將原本的 fb_posts 回傳。
-      if (this.keyword.trim() !== "" || this.select_jobclass.trim() !== "" || this.select_area.trim() !== "") {
+      if (
+        this.keyword.trim() !== "" ||
+        this.select_jobclass.trim() !== "" ||
+        this.select_area.trim() !== ""
+      ) {
         this.searchResult = this.intern_info.filter(function (d) {
-          return d.cp_name.toLowerCase().indexOf(keyword) > -1; //過濾關鍵字
+          return d.title.toLowerCase().indexOf(keyword) > -1; //過濾關鍵字
         });
 
         this.searchResult = this.searchResult.filter(function (d) {
-          return d.cp_name.toLowerCase().indexOf(select_jobclass) > -1; //過濾類別
+          return d.title.toLowerCase().indexOf(select_jobclass) > -1; //過濾類別
         });
 
         this.searchResult = this.searchResult.filter(function (d) {
-          return d.cp_name.toLowerCase().indexOf(select_area) > -1; //過濾地區
-        });
-
-      } else {
-        this.searchResult = this.intern_info;
-      }
-    },
-    //依照關鍵字搜尋貼文
-    filteredPosts: function () {
-      // 因為 JavaScript 的 filter 有分大小寫，
-      // 所以這裡將 keyword 與 intern_info[n].cp_name 通通轉小寫方便比對。
-      var keyword = this.keyword.toLowerCase();
-      var select_jobclass = this.select_jobclass;
-      var select_area = this.select_area;
-      // 如果 filter_name 有內容，回傳過濾後的資料，否則將原本的 fb_posts 回傳。
-      if (this.keyword.trim() !== "" || this.select_jobclass.trim() !== "" || this.select_area.trim() !== "") {
-        this.searchResult = this.intern_info.filter(function (d) {
-          return d.cp_name.toLowerCase().indexOf(keyword) > -1; //過濾關鍵字
-        });
-        this.searchResult = this.searchResult.filter(function (d) {
-          return d.cp_name.toLowerCase().indexOf(select_jobclass) > -1; //過濾類別
-        });
-        this.searchResult = this.searchResult.filter(function (d) {
-          return d.cp_name.toLowerCase().indexOf(select_area) > -1; //過濾地區
+          return d.title.toLowerCase().indexOf(select_area) > -1; //過濾地區
         });
       } else {
         this.searchResult = this.intern_info;
@@ -375,61 +440,59 @@ export default {
       this.currPage = idx;
     },
     //貼文排序由新到舊
-    sortNewtoOld: function(){
-      this.searchResult.sort(function(p1, p2){
-        return (p2.id-p1.id)
+    sortNewtoOld: function () {
+      this.searchResult.sort(function (p1, p2) {
+        return p2.id - p1.id;
       });
     },
     //貼文排序由舊到新
-    sortOldtoNew: function(){
+    sortOldtoNew: function () {
       this.searchResult.sort(function (p1, p2) {
-        return (p1.id-p2.id)
+        return p1.id - p2.id;
       });
     },
     //收藏
-    save: function(p_id){
-      
-      if(this.isLoggedIn){
-        const u_id = this.$store.getters.getUser.ID
-        const api = "api/save"
+    save: function (p_id) {
+      if (this.isLoggedIn) {
+        const u_id = this.$store.getters.getUser.ID;
+        const api = "api/save";
         const params = {
-          p_id:p_id,
-          u_id:u_id
-        }
+          p_id: p_id,
+          u_id: u_id,
+        };
         axios
-          .post(api,params)
+          .post(api, params)
           .then((response) => {
-            alert(response.data.msg)
-            this.saved_posts.splice(this.saved_posts.length, 0, p_id)
+            alert(response.data.msg);
+            this.saved_posts.splice(this.saved_posts.length, 0, p_id);
           })
-          .catch((error) =>{
-            console.log(error)
+          .catch((error) => {
+            console.log(error);
           });
-      }
-      else{
+      } else {
         // 沒有登入會導向登入頁面
-        alert("您尚未登入")
-        this.$router.push("/login")
+        alert("您尚未登入");
+        this.$router.push("/login");
       }
     },
     //取消收藏
-    unsave: function(p_id){
-      const api = "/api/unsave"
-      const u_id = this.$store.getters.getUser.ID
+    unsave: function (p_id) {
+      const api = "/api/unsave";
+      const u_id = this.$store.getters.getUser.ID;
       var params = {
         u_id: u_id,
-        p_id: p_id
-      }
+        p_id: p_id,
+      };
       axios
-      .delete(api,{data:params})
-      .then(response =>{
-        alert(response.data.msg)
-        var index = this.saved_posts.indexOf(p_id)
-        this.saved_posts.splice(index,1)
-      })
-      .catch(error =>{
-        console.log(error)
-      })
+        .delete(api, { data: params })
+        .then((response) => {
+          alert(response.data.msg);
+          var index = this.saved_posts.indexOf(p_id);
+          this.saved_posts.splice(index, 1);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
   created() {
@@ -446,33 +509,28 @@ export default {
         // 請求失敗處理
         console.log(error);
       });
-    
-    if(this.isLoggedIn){
-      api = "/api/show-save"
-      var u_id = this.$store.getters.getUser.ID
-      var params = {u_id: u_id}
+
+    if (this.isLoggedIn) {
+      api = "/api/show-save";
+      var u_id = this.$store.getters.getUser.ID;
+      var params = { u_id: u_id };
       axios
-      .get(api,{params})
-      .then(response =>{
-        
-        //saved_posts只存p_id
-        this.saved_posts = response.data.map(function(items, index){
-          return items.p_id;
+        .get(api, { params })
+        .then((response) => {
+          //saved_posts只存p_id
+          this.saved_posts = response.data.map(function (items, index) {
+            return items.p_id;
+          });
         })
-      })
-      .catch(error =>{
-        console.log(error)
-      })
+        .catch((error) => {
+          console.log(error);
+        });
     }
-    api = "/api/show-top5"
-    axios
-      .get(api)
-      .then(response =>{
-        this.top5_post = response.data;
-      });
-    
+    api = "/api/show-top5";
+    axios.get(api).then((response) => {
+      this.top5_post = response.data;
+    });
   },
-  
 };
 /*export default({
   name: '#user',
@@ -492,7 +550,7 @@ export default {
 })*/
 </script>
 <style>
-  .hidden {
+.hidden {
   display: none;
 }
 #Home a {
--- src/client/views/Save.vue
+++ src/client/views/Save.vue
@@ -5,7 +5,7 @@
       <div class="row align-items-center">
         <div class="col-lg-12 text-left">
           <div class="panel panel-default">
-            <div class="panel-body">
+            <div class="panel-body" style="border-style: ridge">
               <div class="ppp-intern1">
                 <div class="row">
                   <div class="col-lg-2">
@@ -17,18 +17,30 @@
                   </div>
                   <div class="col-lg-10">
                     <p style="font-size: 40px">
-                      <strong>{{user_info.name}}</strong>
+                      <strong>{{ user_info.name }}</strong>
                     </p>
                     <div class="row">
                       <div class="col-lg-4">
-                        <p style="font-size: 20px">性別：{{user_info.gender}}</p>
-                        <p style="font-size: 20px">生日：{{user_info.birth}}</p>
-                        <p style="font-size: 20px">身分證字號：{{user_info.id_card}}</p>
+                        <p style="font-size: 20px">
+                          性別：{{ user_info.gender }}
+                        </p>
+                        <p style="font-size: 20px">
+                          生日：{{ user_info.birth }}
+                        </p>
+                        <p style="font-size: 20px">
+                          身分證字號：{{ user_info.id_card }}
+                        </p>
                       </div>
                       <div class="col-lg-4">
-                        <p style="font-size: 20px">學校：{{user_info.school}}</p>
-                        <p style="font-size: 20px">電話：{{user_info.phone}}</p>
-                        <p style="font-size: 20px">email：{{user_info.email}}</p>
+                        <p style="font-size: 20px">
+                          學校：{{ user_info.school }}
+                        </p>
+                        <p style="font-size: 20px">
+                          電話：{{ user_info.phone }}
+                        </p>
+                        <p style="font-size: 20px">
+                          email：{{ user_info.email }}
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -45,7 +57,7 @@
         <!--eslint-disable-next-line-->
         <div id="Home" v-for="(posts, index) in saved_posts.slice(pageStart, pageStart + countOfPage)" class="posts" >
           <div class="panel panel-default">
-            <div class="panel-body">
+            <div class="panel-body" style="border-style: ridge">
               <div class="row">
                 <div class="col-lg-2">
                   <img
@@ -56,7 +68,9 @@
                 </div>
 
                 <div class="col-lg-10">
-                  <h2 style="font-size: 25px" align="left">{{ posts.title }}</h2>
+                  <h2 style="font-size: 25px" align="left">
+                    {{ posts.title }}
+                  </h2>
                   <p style="font-size: 20px" align="left">
                     {{ posts.cp_name }}
                   </p>
@@ -80,15 +94,64 @@
                 <div class="row float-right">
                   <router-link
                     class="btn"
-                    style="width: 150px; height: 50px; font-size: 20px"
-                    :to="{ name: 'Intern', params: { post_id: posts.id }}"
-                    >立即應徵</router-link>
+                    style="width: 200px; height: 50px; font-size: 20px"
+                    :to="{ name: 'Intern', params: { post_id: posts.id } }"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="18"
+                      fill="currentColor"
+                      class="bi bi-envelope"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z"
+                      /></svg
+                    >&nbsp;立即應徵</router-link
+                  >
+                  <router-link
+                    class="btn"
+                    style="width: 200px; height: 50px; font-size: 20px"
+                    :to="{ name: 'Intern', params: { post_id: posts.id } }"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="18"
+                      fill="currentColor"
+                      class="bi bi-chat-square-text"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+                      />
+                      <path
+                        d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+                      />
+                    </svg>
+                    查看評論
+                  </router-link>
+
                   <button
                     @click="unsave(posts.id)"
                     class="btn"
-                    style="width: 150px; height: 50px; font-size: 20px"
-                    >取消收藏</button
+                    style="width: 200px; height: 50px; font-size: 20px"
                   >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="18"
+                      fill="currentColor"
+                      class="bi bi-suit-heart-fill"
+                      color="red"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                      /></svg
+                    >&nbsp;取消收藏
+                  </button>
                 </div>
               </div>
             </div>
@@ -137,7 +200,7 @@ export default {
   data() {
     return {
       saved_posts: [],
-      user_info:null,
+      user_info: null,
       countOfPage: 5,
       currPage: 1,
     };
@@ -160,41 +223,41 @@ export default {
       }
       this.currPage = idx;
     },
-    setBirth: function(){
-    },
+    setBirth: function () {},
     //取消收藏
-    unsave: function(p_id){
-      const api = "/api/unsave"
-      const u_id = this.user_info.ID
+    unsave: function (p_id) {
+      const api = "/api/unsave";
+      const u_id = this.user_info.ID;
       var params = {
         u_id: u_id,
-        p_id: p_id
-      }
+        p_id: p_id,
+      };
       axios
-      .delete(api,{data:params})
-      .then(response =>{
-        alert(response.data.msg)
-        var index = this.saved_posts.map(function(items, index){
-          return items.p_id;
-        }).indexOf(p_id)
-        this.saved_posts.splice(index,1)
-      })
-      .catch(error =>{
-        console.log(error)
-      })
+        .delete(api, { data: params })
+        .then((response) => {
+          alert(response.data.msg);
+          var index = this.saved_posts
+            .map(function (items, index) {
+              return items.p_id;
+            })
+            .indexOf(p_id);
+          this.saved_posts.splice(index, 1);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
   },
   created() {
     //從store取得使用者資料
-    this.user_info = this.$store.getters.getUser
-    const u_id = this.user_info.ID
+    this.user_info = this.$store.getters.getUser;
+    const u_id = this.user_info.ID;
     const api = "/api/show-save";
     const params = { u_id: u_id };
-    
 
     //axios取得後台收藏貼文資料
     axios
-      .get(api,{params})
+      .get(api, { params })
       .then((response) => {
         this.saved_posts = response.data;
       })
