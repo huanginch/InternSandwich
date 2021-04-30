7562b229e7b7b20e0146763b065afd242271a45c
Shen-KuanLing <44127801+Shen-KuanLing@users.noreply.github.com>
2021-04-30 19:54:40 +0800

    display company post on Home

--- src/client/views/Home.vue
+++ src/client/views/Home.vue
@@ -150,6 +150,197 @@
 
         <!-- 實習貼文 -->
         <!--eslint-disable-next-line-->
+        <div id="Home" v-for="(posts, index) in cp_searchResult.slice(pageStart, pageStart + countOfPage)" class="posts" >
+          <div class="panel panel-default text-left">
+            <div class="panel-body" style="border-style: ridge">
+              <!-- ridge groove inset outset -->
+              <!-- <router-link class="nav-item nav-link" :to="{ name: 'Intern', params: { post_id: posts.id }}"
+            @click.native="addcounter(posts.id, posts.counter)"> -->
+              <div class="row">
+                <div class="col-lg-2">
+                  <img
+                    src="../assets/圖片1.png"
+                    alt="internsandwich"
+                    style="vertical-align: bottom; height: 120px"
+                  />
+                </div>
+                <div class="col-lg-10">
+                  <div class="row">
+                    <div class="col-lg-9">
+                      <span
+                        style="
+                          font-size: 30px;
+                          height: 40px;
+                          overflow: hidden;
+                          text-overflow: ellipsis;
+                          display: -webkit-box;
+                        "
+                        align="left"
+                      >
+                        <strong>{{ posts.title }}</strong>
+                      </span>
+                    </div>
+                    <div class="col-lg-3">
+                      <p
+                        style="
+                          font-size: 15px;
+                          text-align: center;
+                          line-height: 40px;
+                        "
+                        align="center"
+                      >
+                        發佈時間:2020-01-09
+                      </p>
+                    </div>
+                  </div>
+
+                  <p style="font-size: 20px;" align="left">
+                    <router-link to="/businessposts">{{
+                      posts.cp_name
+                    }}</router-link>
+                  </p>
+                  <p
+                    style="
+                      font-size: 15px;
+                      height: 70px;
+                      overflow: hidden;
+                      text-overflow: ellipsis;
+                      display: -webkit-box;
+                      -webkit-line-clamp: 3;
+                      -webkit-box-orient: vertical;
+                    "
+                    align="left"
+                  >
+                    {{ posts.job_desc }}
+                  </p>
+                </div>
+              </div>
+              <div class="ppp2_btn">
+                <div
+                  class="float-left"
+                  style="
+                    font-size: 20px;
+                    color: grey;
+                    padding-top: 12px;
+                    padding-left: 5px;
+                  "
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="32"
+                    fill="currentColor"
+                    class="bi bi-eye-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+                    <path
+                      d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"
+                    />
+                  </svg>
+                  觀看次數: {{ posts.counter }}&nbsp;&nbsp;
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="25"
+                    height="22"
+                    fill="currentColor"
+                    class="bi bi-suit-heart-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                    /></svg
+                  >&nbsp;收藏次數：5
+                </div>
+
+                <div class="row float-right">
+                  <router-link
+                    class="btn"
+                    style="width: 200px; height: 50px; font-size: 20px"
+                    :to="{ name: 'Intern', params: { post_id: posts.id } }"
+                    @click.native="addcounter(posts.id, posts.counter)"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="18"
+                      fill="currentColor"
+                      class="bi bi-chat-square-text"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-2.5a2 2 0 0 0-1.6.8L8 14.333 6.1 11.8a2 2 0 0 0-1.6-.8H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+                      />
+                      <path
+                        d="M3 3.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 6a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 6zm0 2.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"
+                      />
+                    </svg>
+                    查看更多
+                  </router-link>
+                  <div
+                    v-bind:class="{
+                      hidden: saved_posts.indexOf(posts.id) != -1,
+                    }"
+                  >
+                    <button
+                      @click="save(posts.id)"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="18"
+                        fill="currentColor"
+                        class="bi bi-suit-heart"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          d="m8 6.236-.894-1.789c-.222-.443-.607-1.08-1.152-1.595C5.418 2.345 4.776 2 4 2 2.324 2 1 3.326 1 4.92c0 1.211.554 2.066 1.868 3.37.337.334.721.695 1.146 1.093C5.122 10.423 6.5 11.717 8 13.447c1.5-1.73 2.878-3.024 3.986-4.064.425-.398.81-.76 1.146-1.093C14.446 6.986 15 6.131 15 4.92 15 3.326 13.676 2 12 2c-.777 0-1.418.345-1.954.852-.545.515-.93 1.152-1.152 1.595L8 6.236zm.392 8.292a.513.513 0 0 1-.784 0c-1.601-1.902-3.05-3.262-4.243-4.381C1.3 8.208 0 6.989 0 4.92 0 2.755 1.79 1 4 1c1.6 0 2.719 1.05 3.404 2.008.26.365.458.716.596.992a7.55 7.55 0 0 1 .596-.992C9.281 2.049 10.4 1 12 1c2.21 0 4 1.755 4 3.92 0 2.069-1.3 3.288-3.365 5.227-1.193 1.12-2.642 2.48-4.243 4.38z"
+                        />
+                      </svg>
+                      收藏
+                    </button>
+                  </div>
+                  <div
+                    v-bind:class="{
+                      hidden: saved_posts.indexOf(posts.id) === -1,
+                    }"
+                  >
+                    <button
+                      @click="unsave(posts.id)"
+                      class="btn"
+                      style="width: 200px; height: 50px; font-size: 20px"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="18"
+                        fill="currentColor"
+                        class="bi bi-suit-heart-fill"
+                        color="red"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1z"
+                        />
+                      </svg>
+                      取消收藏
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <!-- </router-link> -->
+            </div>
+          </div>
+        </div>
+
+<!--這是用來區隔公司發的貼文跟網路爬來的貼文的分隔線
+      <div>
+        ---------------------------分隔線------------------------------
+      </div>
+-->
         <div id="Home" v-for="(posts, index) in searchResult.slice(pageStart, pageStart + countOfPage)" class="posts" >
           <div class="panel panel-default text-left">
             <div class="panel-body" style="border-style: ridge">
@@ -489,6 +680,8 @@ export default {
       top5_post: null,
       intern_info: null,
       searchResult: null,
+      company_posts: null,
+      cp_searchResult: null,
       countOfPage: 15,
       currPage: 1,
       select_jobclass: null,
@@ -651,7 +844,7 @@ export default {
     },
     //設定總頁數
     totalPage: function () {
-      return Math.ceil(this.searchResult.length / this.countOfPage);
+      return Math.ceil((this.searchResult.length + this.cp_searchResult.length) / this.countOfPage);
     },
     isLoggedIn: function () {
       return this.$store.getters.isLoggedIn;
@@ -698,12 +891,22 @@ export default {
         this.searchResult = this.intern_info.filter(function (d) {
           return d.title.toLowerCase().indexOf(keyword) > -1; //過濾關鍵字
         });
+        this.cp_searchResult = this.company_posts.filter(function (d) {
+          return d.title.toLowerCase().indexOf(keyword) > -1; //過濾關鍵字
+        });
         // console.log(this.keyword.trim() !== "");
         // console.log(select_jobclass);
         // console.log(select_area);
 
         // 0426問題  cp_name無法搜尋
         this.searchResult = this.intern_info.filter(function (d) {
+          return (
+            d.cp_name.toLowerCase().indexOf(keyword) > -1 ||
+            d.job_desc.toLowerCase().indexOf(keyword) > -1 ||
+            d.cp_name.toLowerCase().indexOf(keyword) > -1
+          ); //過濾關鍵字
+        });
+        this.cp_searchResult = this.company_posts.filter(function (d) {
           return (
             d.cp_name.toLowerCase().indexOf(keyword) > -1 ||
             d.job_desc.toLowerCase().indexOf(keyword) > -1 ||
@@ -718,8 +921,17 @@ export default {
         this.searchResult = this.searchResult.filter(function (d) {
           return d.title.toLowerCase().indexOf(select_jobclass) > -1; //過濾類別
         });
+        this.cp_searchResult = this.searchResult.filter(function (d) {
+          return d.title.toLowerCase().indexOf(select_jobclass) > -1; //過濾類別
+        });
 
         this.searchResult = this.searchResult.filter(function (d) {
+          return (
+            d.title.toLowerCase().indexOf(select_area) > -1 ||
+            d.job_desc.toLowerCase().indexOf(select_area) > -1
+          ); //過濾地區
+        });
+        this.cp_searchResult = this.searchResult.filter(function (d) {
           return (
             d.title.toLowerCase().indexOf(select_area) > -1 ||
             d.job_desc.toLowerCase().indexOf(select_area) > -1
@@ -731,6 +943,7 @@ export default {
         // });
       } else {
         this.searchResult = this.intern_info;
+        this.cp_searchResult = this.company_posts;
       }
     },
     //設定當前頁面
@@ -745,16 +958,25 @@ export default {
       this.searchResult.sort(function (p1, p2) {
         return p2.id - p1.id;
       });
+      this.cp_searchResult.sort(function (p1, p2) {
+        return p2.id - p1.id;
+      });
     },
     //貼文排序由舊到新
     sortOldtoNew: function () {
       this.searchResult.sort(function (p1, p2) {
         return p1.id - p2.id;
       });
+      this.cp_searchResult.sort(function (p1, p2) {
+        return p1.id - p2.id;
+      });
     },
     //觀看次數排序由多到少
     sortPopularity: function () {
       this.searchResult.sort(function (p1, p2) {
+        return p2.counter - p1.counter;
+      });
+      this.cp_searchResult.sort(function (p1, p2) {
         return p2.counter - p1.counter;
       });
     },
@@ -811,6 +1033,20 @@ export default {
       .then((response) => {
         this.intern_info = response.data;
         this.searchResult = response.data;
+      })
+      .catch(function (error) {
+        // 請求失敗處理
+        console.log(error);
+      });
+
+    //顯示公司發佈之實習貼文
+    var api = "/api/company_posts";
+    //const params = { userId: 2 };
+    axios
+      .get(api)
+      .then((response) => {
+        this.company_posts = response.data;
+        this.cp_searchResult = response.data;
       })
       .catch(function (error) {
         // 請求失敗處理
